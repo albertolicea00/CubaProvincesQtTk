b88de12c2f0b9d79bc3ee2182e602651209154d4
albertolicea00 <albertolicea00@icloud.com>
2023-04-21 12:30:00 -0400

    refactor(examples): use explicit imports and document each step
    Replace 'from ... import *' with explicit names in both examples, drop
    the unused QtCore/QtGui imports, and add per-line comments. example2 no
    longer creates redundant combo boxes, since Mode B builds its own.

--- qt.example1.py
+++ qt.example1.py
@@ -1,20 +1,43 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+"""Example 1 - caller-managed combo boxes (Mode A).
 
-from qt import *
+You create the province and municipality combo boxes yourself, then hand both
+to ``CubaProvinces_BoxGroup``. The group fills the province box and links the
+municipality box so it refreshes every time the selected province changes.
 
+Run with:  python qt.example1.py
+"""
+
+# Import only the widgets this example actually uses (no wildcard imports):
+#   QApplication - owns the Qt event loop (one per program)
+#   QMainWindow  - the top-level window
+#   QComboBox    - the two dropdowns we fill
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox
+
+# The helper class under test; imported by name instead of "from qt import *".
+from qt import CubaProvinces_BoxGroup
+
+# Every Qt program needs exactly one QApplication. The list is sys.argv-style
+# command-line args; an empty list is fine for a demo.
 app = QApplication([])
+
+# Top-level window that will host the two combo boxes.
 root = QMainWindow()
 
+# Mode A: we build the combo boxes ourselves and parent them to the window.
 cb_provinces = QComboBox(root)
 cb_municipality = QComboBox(root)
-cb_provinces.setGeometry(0,0,200,30)
-cb_municipality.setGeometry(0,32,200,30)
 
-# example 1
-a = CubaProvinces_BoxGroup(provinceBox = cb_provinces , municipalityBox=cb_municipality)
+# Position each box as (x, y, width, height): provinces on top, municipalities
+# 32 px below it.
+cb_provinces.setGeometry(0, 0, 200, 30)
+cb_municipality.setGeometry(0, 32, 200, 30)
+
+# Hand both boxes to the group. exec() fills the province box and wires the
+# municipality box to follow whichever province is selected.
+a = CubaProvinces_BoxGroup(provinceBox=cb_provinces, municipalityBox=cb_municipality)
 a.exec()
 
+# Show the window, then start the event loop. exec_() blocks until the window
+# is closed.
 root.show()
 app.exec_()
--- qt.example2.py
+++ qt.example2.py
@@ -1,20 +1,35 @@
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+"""Example 2 - group-built combo boxes (Mode B).
 
-from qt import *
+Instead of creating the combo boxes yourself, pass a ``parent`` widget and an
+``allignment``. ``CubaProvinces_BoxGroup`` then creates, positions, fills, and
+links both combo boxes for you.
 
+Run with:  python qt.example2.py
+"""
+
+# Import only the widgets this example actually uses (no wildcard imports).
+# Note there is no QComboBox here: in Mode B the group builds the boxes itself.
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+# The helper class under test; imported by name instead of "from qt import *".
+from qt import CubaProvinces_BoxGroup
+
+# Every Qt program needs exactly one QApplication. The list is sys.argv-style
+# command-line args; an empty list is fine for a demo.
 app = QApplication([])
+
+# Top-level window. We pass it to the group as the parent for the boxes it
+# builds.
 root = QMainWindow()
 
-cb_provinces = QComboBox(root)
-cb_municipality = QComboBox(root)
-cb_provinces.setGeometry(0,0,200,30)
-cb_municipality.setGeometry(0,32,200,30)
-
-# example 2
-a = CubaProvinces_BoxGroup(parent = root , allignment = "row")
+# Mode B: give the group a parent and a layout direction; it does the rest.
+#   allignment="row"    -> boxes stacked (province above municipality)
+#   allignment="column" -> boxes side by side
+# exec() builds both boxes, fills the province box, and links them together.
+a = CubaProvinces_BoxGroup(parent=root, allignment="row")
 a.exec()
 
+# Show the window, then start the event loop. exec_() blocks until the window
+# is closed.
 root.show()
 app.exec_()
